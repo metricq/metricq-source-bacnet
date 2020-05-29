8761dd41487d24842c9fef43b0853d9e1fc3d7a1
Franz Hoepfner <franz.hoepfner@tu-dresden.de>
2020-05-29 10:20:10 +0200

    Use OptionalProperty from bacpypes instead of own implementation

--- metricq_source_bacnet/bacnet/object_types.py
+++ metricq_source_bacnet/bacnet/object_types.py
@@ -1,24 +1,23 @@
-from bacpypes.object import (AnalogInputObject, AnalogValueObject, Property,
-                             register_object_type)
+from bacpypes.object import (AnalogInputObject, AnalogValueObject,
+                             OptionalProperty, register_object_type)
 from bacpypes.primitivedata import CharacterString
 
-
-class CustomOptionalProperty(Property):
-
-    """The property is optional and need not be present."""
-
-    def __init__(
-        self, identifier, datatype, default=None, optional=True, mutable=False
-    ):
-        Property.__init__(self, identifier, datatype, default, optional, mutable)
+# class CustomOptionalProperty(Property):
+#
+#     """The property is optional and need not be present."""
+#
+#     def __init__(
+#         self, identifier, datatype, default=None, optional=True, mutable=False
+#     ):
+#         Property.__init__(self, identifier, datatype, default, optional, mutable)
 
 
 class ExtendedAnalogInputObject(AnalogInputObject):
-    properties = [CustomOptionalProperty(3000, CharacterString)]
+    properties = [OptionalProperty(3000, CharacterString)]
 
 
 class ExtendedAnalogValueObject(AnalogValueObject):
-    properties = [CustomOptionalProperty(3000, CharacterString)]
+    properties = [OptionalProperty(3000, CharacterString)]
 
 
 def register_extended_object_types():
