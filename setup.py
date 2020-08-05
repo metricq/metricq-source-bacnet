5382c178272829071dcae01c8a74986eaecb992c
Franz Hoepfner <franz.hoepfner@tu-dresden.de>
2020-08-05 12:45:06 +0200

    Bump metricq version and depend on same major version instead of same minor version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         "click",
         "click-completion",
         "click_log",
-        "metricq~=1.1.0",
+        "metricq~=1.2",
         "bacpypes~=0.18.0",
     ],
     extras_require={"journallogger": ["systemd"]},
